d73900808559e59fdd5df44ddff27c002a3d106d
unknown <koptevviktor1997@gmail.com>
2018-09-29 18:49:12 +0300

    Search added

--- src/components/AllTasks.jsx
+++ src/components/AllTasks.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Task } from './Task';
 
 export const AllTasks = (props) => props.allTasks.map((task, index) =>
-    <Task
-        key={index}
-        titleName={task.TaskTitle}
-        projectName={task.ProjectName}
-        currentSeconds={task.Time}
-    />
+    <div className="task">
+      <Task
+          key={index}
+          titleName={task.TaskTitle}
+          projectName={task.ProjectName}
+          currentSeconds={task.Time}
+          id={task.id}
+      />
+    </div>
 )
